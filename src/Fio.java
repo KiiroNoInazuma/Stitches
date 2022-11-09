2abb6631a5d70eb5ab1e694eb89226681ef6e105
John Rey <adioskid@mail.ru>
2022-11-10 00:16:05 +0300

    Homework#6

--- src/Fio.java
+++ src/Fio.java
@@ -1,5 +1,11 @@
 public class Fio {
     public static void main(String[] args) {
-        
+        String firstName = "Ivanov ";
+        String middleName = "Ivan ";
+        String lastName = "Ivanovich";
+        String fullName = firstName+middleName+lastName;
+        System.out.println("ФИО сотрудника — " +fullName);
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName);
     }
 }
