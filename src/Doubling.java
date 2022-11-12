af44ee13f4191878313418357432504ed0ce20f6
John Rey <adioskid@mail.ru>
2022-11-12 22:47:50 +0300

    Homework#6

---
+++ src/Doubling.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Doubling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String doubleChar = sc.nextLine();
+        // variant #1
+        for (int i = 0; i < doubleChar.length(); i++) {
+            System.out.print(doubleChar.charAt(i));
+            System.out.print(doubleChar.charAt(i));
+        }
+        //variant #2
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < doubleChar.length(); i++) {
+            sb.append(doubleChar.charAt(i));
+            sb.append(doubleChar.charAt(i));
+        }
+        System.out.println("\n" + sb);
+    }
+}
