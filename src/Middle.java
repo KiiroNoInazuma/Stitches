0b139e1546ea0293263b171106cace36b0e0bed2
John Rey <adioskid@mail.ru>
2022-11-13 22:42:30 +0300

    Homework#6

--- src/Middle.java
+++ src/Middle.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
+
 public class Middle {
     public static void main(String[] args) {
-        String middle = "[<<>>]";
-        String makeOutWord = "Yay";
+        Scanner scanner = new Scanner(System.in);
+        String middle = "<<>>";
+        String makeOutWord = scanner.nextLine();
         // Variant #1
         StringBuilder sb = new StringBuilder(middle);
-        sb.insert(middle.length()/2, makeOutWord);
+        sb.insert(middle.length() / 2, makeOutWord);
         System.out.println(sb);
         // Variant #2
-        String result = middle.substring(0,(middle.length()/2))+makeOutWord+middle.substring(middle.length()/2);
+        String result = middle.substring(0, (middle.length() / 2)) + makeOutWord + middle.substring(middle.length() / 2);
         System.out.println(result);
     }
 }
