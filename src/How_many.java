9f838848813708ccd2b0ea1f7e40efc556946356
John Rey <adioskid@mail.ru>
2022-11-14 19:59:59 +0300

    Homework#6

---
+++ src/How_many.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class How_many {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите исходный текст: ");
+        String text = sc.nextLine();
+        System.out.print("Введите текст для поиска: ");
+        String countHi = sc.nextLine();
+        countHi = countHi.toUpperCase();
+        text = text.toUpperCase();
+        int check = 0;
+        for (int x = 0; x < text.length() - 1; x++) {
+            if (countHi.equals(text.substring(x, x + countHi.length()))) {
+                check++;
+            }
+        }
+
+        System.out.println("Текст встречается " + check + " раз(а).");
+    }
+}
+
