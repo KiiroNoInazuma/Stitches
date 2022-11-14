fbbd4f25d0a20d3d5d88b79d224efcbaf3e87b9b
John Rey <adioskid@mail.ru>
2022-11-14 22:00:12 +0300

    Homework#6

--- src/Repeatedly.java
+++ src/Repeatedly.java
@@ -1,13 +1,5 @@
 public class Repeatedly {
     public static void main(String[] args) {
-        String str = "123";
-        String str1 = "1234";
-        try {
-            int result = str1.charAt(10);
-        } catch (RuntimeException e) {
-            System.out.println("error");
-        }
-
-
     }
 }
+
