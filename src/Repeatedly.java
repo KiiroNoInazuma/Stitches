3eec465b8eafa3270f5790b0ee3ecbaba053183c
John Rey <adioskid@mail.ru>
2022-11-15 16:58:10 +0300

    Homework#6

--- src/Repeatedly.java
+++ src/Repeatedly.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Repeatedly {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = "Hello";
+        int result = sc.nextInt();
+        try {
+            for (int x = 0; x < result; x++) {
+                word = word.substring(word.length() - result);
+                System.out.print(word);
+            }
+        } catch (StringIndexOutOfBoundsException ind) {
+            System.out.println("Нельзя ввести число больше " + word.length());
+            main(args);
+        }
     }
 }
+
+
 
